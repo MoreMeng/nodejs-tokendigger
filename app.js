bebdad56cb13c785b24cb3d84ce67cc306671f95
Thanikul Sriuthis <moremeng@dv4.biz>
2020-11-12 14:15:54 +0700

    use watch  than watchFile

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 const fs = require('fs')
+const md5 = require('md5')
 const http = require('http')
 const dotenv = require('dotenv').config()
 
 const nhsoToken = process.env.TOKENPATH
 console.log(`Watching for file changes on ${nhsoToken}`)
 
-fs.watchFile(nhsoToken, (curr, prev) => {
-  const token = (fs.readFileSync(nhsoToken) + '').replace(/(\r\n|\n|\r|\\n|\s|\t)/gm, '')
-  console.log(`${token}`)
+let md5Previous = null
+let fsWait = false
 
-  const options = {
-    hostname: process.env.SERV,
-    port: process.env.PORT,
-    path: '/token',
-    method: 'POST',
+fs.watch(nhsoToken, (event, filename) => {
+  if (filename) {
+    if (fsWait) return
+    fsWait = setTimeout(() => {
+      fsWait = false
+    }, 100)
+    const md5Current = md5(fs.readFileSync(nhsoToken))
+    if (md5Current === md5Previous) {
+      return
+    }
+    md5Previous = md5Current
+
+    console.log(`${filename} file Changed`)
+
+    const token = (fs.readFileSync(nhsoToken) + '').replace(/(\r\n|\n|\r|\\n|\s|\t)/gm, '')
+    console.log(`${token}`)
+
+    // const options = {
+    //   hostname: process.env.SERV,
+    //   port: process.env.PORT,
+    //   path: '/token',
+    //   method: 'POST',
+    // }
+    // const req = http.request(options, res => {
+    //   console.log(`statusCode: ${res.statusCode}`)
+    //   res.on('data', d => {
+    //     process.stdout.write(d)
+    //   })
+    // })
+
+    // req.on('error', error => {
+    //   console.error(error)
+    // })
+
+    // req.write(token)
+    // req.end()
   }
-  const req = http.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-    res.on('data', d => {
-      process.stdout.write(d)
-    })
-  })
-
-  req.on('error', error => {
-    console.error(error)
-  })
-
-  req.write(token)
-  req.end()
 })
